ec38e1449aef929c861781fee1643ef70ac53722
Andre Fishy McGee <5317855+potatofish@users.noreply.github.com>
2020-07-31 22:10:11 -0700

    tests for Action classes

--- src/v0.0.0/Action/test/test.js
+++ src/v0.0.0/Action/test/test.js
@@ -1,35 +1,71 @@
 const assert = require('assert');
 const ActionManager = require('../ActionManager');
+const ActionFactory = require('../ActionFactory');
+const Action = require('../Action');
 
-describe('ActionManager', function () {
-    describe('Constructor', function () {
-        it('Test stuff', function () {
+describe('ActionManager',  () => {
+    describe('constructor()', () => {
+        it('ActionManager has an ActionFactory to make Actions with', () => {
             let actionManager = new ActionManager();
 
-            let foo = "Foo"
+            let isActionFactory = actionManager._objectFactory instanceof ActionFactory
 
-            actionManager.manage(foo);
+            assert.ok(isActionFactory);
+        });
+    });
 
-            let fooKey = actionManager.keysOf(foo)[0];
+    describe("create()", () => {
+        it("ActionManager can make Actions", ()=> {
+            let actionManager = new ActionManager();
+            
+            let action = actionManager.create();
 
-            fooKeyValue = actionManager.atKey(fooKey)
+            let isAction = action instanceof Action;
 
-            assert.equal(fooKeyValue, foo);
+            assert.ok(isAction);
         });
 
-        it('Test Other stuff', function () {
+        it('ActionManager makes Action matching function provided', () => {
             let actionManager = new ActionManager();
 
-            let foo = "Foo2"
+            const woofString = "woof";
+            const woof = (a,b) => {return woofString + a + b;};
+            
+            let action = actionManager.create(woofString, woof);
 
-            actionManager.manage(foo);
-
-            let fooKey = actionManager.keysOf(foo)[0];
-
-            fooKeyValue = actionManager.atKey(fooKey)
-
-            assert.equal(fooKeyValue, foo);
+            assert.equal(woof(1,2), action.execute(1,2));
         });
+
+        it('ActionManager makes Action matching name provided', () => {
+            let actionManager = new ActionManager();
+
+            const woofString = "woof";
+            
+            let action = actionManager.create(woofString, ()=>{});
+
+            assert.equal(woofString, action.name);
+        });
+
+        it('ActionManager makes Action matching (only) name provided', () => {
+            let actionManager = new ActionManager();
+
+            const woofString = "woof";
+            
+            let action = actionManager.create(woofString);
+
+            assert.equal(woofString, action.name);
+        });
+    });
+
+    describe('manage()', () => {
+        
     });
 });
 
+describe('ActionFactory', () => {
+    
+});
+
+describe('Action', () => {
+    
+});
