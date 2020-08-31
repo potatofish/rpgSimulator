c4f7336ad4f9119dc4575d0b3037b3b181540c2c
Andre Fishy McGee <5317855+potatofish@users.noreply.github.com>
2020-08-31 12:24:46 -0700

    revised script to accomodate memory

--- src/v0.0.1/Design/brainstorm/buildpdf.sh
+++ src/v0.0.1/Design/brainstorm/buildpdf.sh
@@ -6,5 +6,19 @@
  # References:  
  #  How to Install ImageMagick for PHP on Ubuntu 18.04 
  #  https://www.serverlab.ca/tutorials/linux/administration-linux/how-to-install-imagemagick-for-php-on-ubuntu-18-04/
- set -x
- convert *.jpg "How To Play.pdf"
+set +x
+#find . -maxdepth 1 -type f -name "*.jpg" > imageJPG_list.tmp
+
+#fileList="./imageJPG_list.tmp"
+tmpDirectory="./convert/"
+finalPDF="./How To Play.pdf"
+
+rm -f "$finalPDF"
+touch "$finalPDF"
+
+mkdir  "$tmpDirectory"
+
+convert -define "registry:temporary-path=$tmpDirectory" -limit memory 2gb slides/*.jpg[1600x900] "$finalPDF"
+
+rm -r "$tmpDirectory"
+
