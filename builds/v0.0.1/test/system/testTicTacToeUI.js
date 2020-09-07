b869a861370e7c4dc48c59c90cbf6d60e151ac19
Andre Fishy McGee <5317855+potatofish@users.noreply.github.com>
2020-09-06 21:04:48 -0700

    system testing starting poiint

---
+++ builds/v0.0.1/test/system/testTicTacToeUI.js
@@ -0,0 +1,36 @@
+/*jshint node: true, esversion: 9*/
+"use strict";
+
+const assert = require('assert');
+/*
+const { AssertionError } = require('assert');
+const GameSimulation = require('../../src/GameSimulation/GameSimulation.js');
+const User = require('../../src/GameSimulation/User.js');
+
+
+describe('GameSimulation', () => {
+    describe('constructor()', () => {
+        it('The GameSimulation is uninitialized', () => {
+            const aGameSimulation = new GameSimulation();
+            const earlyInitMessage = "isInitialized w/o init()";
+            assert(!(aGameSimulation.isInitialized), earlyInitMessage);
+
+            aGameSimulation.init();
+            const notInitMessage = "isInitialized w/o init()";
+            assert(aGameSimulation.isInitialized, notInitMessage);
+        });
+
+        it('The GameSimulation can load a GameSystem', () => {
+            const aGameSimulation = new GameSimulation();
+            const earlyInitMessage = "isInitialized w/o init()";
+            assert(!(aGameSimulation.isInitialized), earlyInitMessage);
+
+            aGameSimulation.init();
+            const notInitMessage = "isInitialized w/o init()";
+            assert(aGameSimulation.isInitialized, notInitMessage);
+        
+            aGameSimulation.load(aGameSystem);
+        });
+    });
+});
+*/
